2f5b43f266800ed9097c5915e9d1704d86f6459e
ozlemakbas <ozlemakbas90@gmail.com>
2015-12-13 15:40:20 +0200

    Initial skeletal code for PyQt
    Initial skeletal code for PyQt

--- client.py
+++ client.py
@@ -5,8 +5,8 @@
 import sys
 import socket
 import threading
-#from PyQt4.QtCore import *
-#from PyQt4beyh.QtGui import *
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 import Queue
 import time
 
@@ -153,11 +153,71 @@ class WriteThread (threading.Thread):
                 self.csoc.close()
                 #break
 
-class ClientDialog():
+class ClientDialog(QDialog):
 
     def __init__(self, threadQueue, screenQueue):
         self.threadQueue = threadQueue
         self.screenQueue = screenQueue
+
+        # create a Qt application --- every PyQt app needs one
+        self.qt_app = QApplication(sys.argv)
+
+        # Call the parent constructor on the current object
+        QDialog.__init__(self, None)
+
+        # Set up the window
+        self.setWindowTitle('IRC Client')
+        self.setMinimumSize(500, 200)
+        self.resize(640, 480)
+
+        # Add a vertical layout
+        self.vbox = QVBoxLayout()
+        self.vbox.setGeometry(QRect(10, 10, 621, 461))
+
+        # Add a horizontal layout
+        self.hbox = QHBoxLayout()
+
+        # The sender textbox
+        self.sender = QLineEdit("", self)
+
+        # The channel region
+        self.channel = QTextBrowser()
+        self.channel.setMinimumSize(QSize(480, 0))
+
+        # The send button
+        self.send_button = QPushButton('&Send')
+
+        # The users' section
+        self.userList = QListView()
+
+        # Connect the Go button to its callback
+        self.send_button.clicked.connect(self.outgoing_parser)
+
+        # Add the controls to the vertical layout
+        self.vbox.addLayout(self.hbox)
+        self.vbox.addWidget(self.sender)
+        self.vbox.addWidget(self.send_button)
+        self.hbox.addWidget(self.channel)
+        self.hbox.addWidget(self.userList)
+
+        # start timer
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.updateChannelWindow)
+
+        # update every 10 ms
+        self.timer.start(10)
+
+        # Use the vertical layout for the current window
+        self.setLayout(self.vbox)
+
+    def cprint(self, data):
+        self.channel.append(data)
+
+
+    def updateChannelWindow(self):
+        if self.screenQueue.qsize() > 0:
+            queue_message = self.screenQueue.get()
+            # self.channel.append(...)
 
     #ToDO: GUI part will be implemented after Qt installation
     def outgoing_parser(self):
@@ -199,9 +259,19 @@ class ClientDialog():
             self.threadQueue.put("SAY " + data)
         self.sender.clear()
 
+    #Run the app and show the main form
+    def run(self):
+        self.show()
+        self.qt_app.exec_()
+
 # Connect to the server
 s = socket.socket()
 host = "178.233.19.205"
 port = 12345
 s.connect((host,port))
 print s.recv(1024)      #To check server connection will be deleted
+
+sendQueue = Queue.Queue()
+screenQueue = Queue.Queue()
+app = ClientDialog(sendQueue, screenQueue)
+app.run()
